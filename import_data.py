dc93525b27a7fd1b25e7466e58917cc08aa7ccb6
Shihab Khan <shihabkhan1@gmail.com>
2022-01-09 12:47:25 +0530

    import_data.py: initial commit

---
+++ import_data.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  9 11:38:15 2022
+
+@author: shihab
+"""
+
+import pandas as pd
+import numpy as np
+import pickle as pkl
+
+def import_data(file, skipr=None, headr=None, colindex=None):
+    """
+    This function can import data from an excel or a csv file and return a
+    pandas dataframe.
+
+    Parameters
+    ----------
+    file : string
+        Complete path to the file.
+    skipr : list of int, optional
+        The rows that need to be skipped beginning from 0. The default is None.
+    headr : list of int, optional
+        Specify the header rows. The default is None.
+    colindex : list of int, optional
+        Specify the indices of the columns. The default is None.
+
+    Raises
+    ------
+    Exception
+        If the filetype is not recognized, an exception is raised.
+
+    Returns
+    -------
+    dfs : Dictionary of dataframes
+        Pandas dataframe from the read file.
+
+    """
+    if file.endswith('.xlsx'):
+        dfs = pd.read_excel(file, skiprows=skipr, header=headr,\
+                             index_col=colindex, sheet_name=None)
+    elif file.endswith('.csv'):
+        pass
+    else:
+        raise Exception("Sorry, filetype not recognized")
+
+
+    return dfs
